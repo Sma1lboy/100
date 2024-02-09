873284f4e119759812791ba3020fa81500fc887f
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-02-08 19:43:05 -0600

    to: adding hot reload, and fixing route

--- src/main.js
+++ src/main.js
@@ -4,16 +4,17 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import './main.css'
+import index from './index.vue'
 const app = createApp(App);
 
 import routes from '~pages'
+
+routes.push({ path: '/', component: index })
+
 console.log(routes)
 const router = createRouter({
-    
-
     history: createWebHistory(),
     routes
-
 })
 app.use(router)
 
--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: "localhost", // 指定服务器主机名
+    port: 8880, // 指定服务器端口
+    hmr: true,  // 开启热更新
+    open: true, // 在服务器启动时自动在浏览器中打开应用程序
+    https: false // 是否开启 https
   }
 })
